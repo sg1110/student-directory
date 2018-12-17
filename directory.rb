cc18153bdd0b1d35a82390bd68027007211c0e5b
your name <saule.guzyte@gmail.com>
2018-12-17 11:28:04 +0000

    Excercise 8.1, index added

--- directory.rb
+++ directory.rb
@@ -17,8 +17,9 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student,index|
+    index +=1
+    puts "#{index}. #{student[:name] } (#{student[:cohort]} cohort)"
   end
 end
 
